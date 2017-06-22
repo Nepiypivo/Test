d324f811f1ca17c64cc5357eac534be2098921de
Sergey Babanov <nepiypivo@gmail.com>
2017-06-22 20:45:25 +0300

    job commit

---
+++ Test/UserRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public  class UserRepository
+    {
+        public User GetUser(string login)
+        {
+            using (TestEntities dbContext = new TestEntities())
+            {
+                var user =new User();
+                try
+                {
+                    user = dbContext.Users.First(p => p.Login == login);
+                }
+                catch (Exception)
+                {
+                   // write to file logout.log
+                }
+                return user;
+            }
+                
+        }
+        public User GetUser(int id)
+        {
+            using (TestEntities dbContext = new TestEntities())
+            {
+                var user = new User();
+                try
+                {
+                    user = dbContext.Users.First(p => p.Id==id);
+                }
+                catch (Exception)
+                {
+                    // write to file logout.log
+                }
+                return user;
+            }
+
+        }
+        public int SaveUser(User user)
+        {
+            using (TestEntities dbContext = new TestEntities())
+            {
+                var ExistUser = new User();
+                ExistUser = GetUser(user.Login);
+                if (ExistUser.Id==0)
+                {
+                    dbContext.Users.Add(user);
+                    dbContext.SaveChanges();
+                    return 0;
+                } else
+                {
+                    return -1;
+                }
+                
+            }
+        }
+    }
+}
